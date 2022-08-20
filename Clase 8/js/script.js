57023f4bf598d2a174d31882c2480ba68d16b166
Díaz Sebastián Alberto <Hazachiel@gmail.com>
2022-08-20 16:23:32 -0300

    Add files via upload

---
+++ Clase 8/js/script.js
@@ -0,0 +1,15 @@
+let usuario = document.getElementById("usuario");
+let contraseña = document.getElementById("contraseña");
+let email = document.getElementById("email");
+let genero = document.getElementById("genero");
+
+
+function funcion1() {
+    let generoSeleccionado = genero.options[genero.selectedIndex].value;
+    alert(usuario.value);
+    alert(generoSeleccionado);
+}
+function fhghh () {
+    
+}
+
